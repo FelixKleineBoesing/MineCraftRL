0e1adf6e9bffbb6b2196065c38ea3ed39744bedb
Felix Kleine Bösing <felix.boesing@t-online.de>
2019-09-04 21:04:48 +0200

    added replay buffer and q löearning agent from checkersai

--- src/ScratchDataSet.py
+++ src/ScratchDataSet.py
@@ -2,22 +2,29 @@ import minerl
 import os
 # YOU ONLY NEED TO DO THIS ONCE!
 #minerl.data.download("data/")
-data = minerl.data.make('MineRLObtainDiamond-v0', data_dir='D:\CodingProjects\MinrCraftRL\data')
 
-# Iterate through a single epoch gathering sequences of at most 32 steps
-for current_state, action, reward, next_state, done \
-    in data.sarsd_iter(
-        num_epochs=1, max_sequence_len=32):
 
-        # Print the POV @ the first step of the sequence
-        print(current_state['pov'][0])
+def main():
+        data = minerl.data.make('MineRLObtainDiamond-v0', data_dir='D:\CodingProjects\MinrCraftRL\data')
 
-        # Print the final reward pf the sequence!
-        print(reward[-1])
+        # Iterate through a single epoch gathering sequences of at most 32 steps
+        for current_state, action, reward, next_state, done \
+            in data.sarsd_iter(
+                num_epochs=1, max_sequence_len=32):
 
-        # Check if final (next_state) is terminal.
-        print(done[-1])
+                # Print the POV @ the first step of the sequence
+                print(current_state['pov'][0])
 
-        # ... do something with the data.
-        print("At the end of trajectories the length"
-              "can be < max_sequence_len", len(reward))
+                # Print the final reward pf the sequence!
+                print(reward[-1])
+
+                # Check if final (next_state) is terminal.
+                print(done[-1])
+
+                # ... do something with the data.
+                print("At the end of trajectories the length"
+                      "can be < max_sequence_len", len(reward))
+
+
+if __name__ == "__main__":
+        main()
--- src/ScratchEnv.py
+++ src/ScratchEnv.py
@@ -1,11 +1,17 @@
 import minerl
 import gym
 
-env = gym.make('MineRLNavigateDense-v0')
 
-obs = env.reset()
+def main():
+    env = gym.make('MineRLNavigateDense-v0')
 
-done = False
-while not done:
-    action = env.action_space.sample()
-    obs, reward, done, info = env.step(action)
+    obs = env.reset()
+
+    done = False
+    while not done:
+        action = env.action_space.sample()
+        obs, reward, done, info = env.step(action)
+
+
+if __name__ == '__main__':
+    main()
